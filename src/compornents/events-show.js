60ce265a1ea685cf9d089523edcae26d529507fa
takakou19 <vvkonovv@gmail.com>
2020-08-02 18:09:47 +0900

    新規作成画面、更新画面にmaterial-uiを適用 更新画面のconsole.logをコメントアウト

--- src/compornents/events-show.js
+++ src/compornents/events-show.js
@@ -2,6 +2,9 @@ import React, {Component} from 'react';
 import { connect } from 'react-redux';
 import { Field, reduxForm} from "redux-form";
 import { Link } from 'react-router-dom'
+import RaisedButton from 'material-ui/RaisedButton'
+import TextField from 'material-ui/TextField'
+
 
 import { deleteEvent, getEvent, putEvent } from '../actions';
 // import mapStateToProps from "react-redux/lib/connect/mapStateToProps";
@@ -9,27 +12,31 @@ import { deleteEvent, getEvent, putEvent } from '../actions';
 class EventsShow extends Component {
     constructor(props) {
         // console.log("events-showのid:"+props.match.params.id)
-        console.log("EventShowのconstructor")
+        // console.log("EventShowのconstructor")
         super(props)
         this.onSubmit = this.onSubmit.bind(this)
         this.onDeleteClick = this.onDeleteClick.bind(this)
     }
 
     componentDidMount() {
-        console.log("EventShowのcomponentDidMount")
+        // console.log("EventShowのcomponentDidMount")
         const { id } = this.props.match.params
         if (id) this.props.getEvent(id)
     }
 
     renderField(Field){
-        console.log("events.showのrenderField()")
+        // console.log("events.showのrenderField()")
         const { input, label, type, meta:{ touched, error }} = Field
 
         return (
-            <div>
-                <input {...input} placeholder={label} type={type} />
-                {touched && error && <span>{error}</span>}
-            </div>
+            <TextField
+                hintText={label}
+                floatingLabelText={label}
+                type={type}
+                errorText={touched && error}
+                {...input}
+                fullWidth={true}
+            />
         )
     }
 
@@ -47,17 +54,18 @@ class EventsShow extends Component {
 
     render() {
         const { handleSubmit, pristine, submitting, invalid } = this.props
+        const style ={margin:12}
+
         return (
             <form onSubmit={handleSubmit(this.onSubmit)}>
                 <div>
                     <div><Field label="Title" name='title' type='text' component={this.renderField}></Field></div>
                     <div><Field label="Body" name='body' type='text' component={this.renderField}></Field></div>
 
-                    <div>
-                        <input type="submit" value="submit" disabled={pristine || submitting || invalid}/>
-                        <Link to="/">Cancel</Link>
-                        <Link to="/" onClick={this.onDeleteClick}>Delete</Link>
-                    </div>
+
+                    <RaisedButton label="Submit" type="submit" style={style} disabled={pristine || submitting || invalid}/>
+                    <RaisedButton label="Cancel" style={style} containerElement={<Link to="/"/>} />
+                    <RaisedButton label="Delete" style={style} onClick={this.onDeleteClick} />
 
                 </div>
             </form>
@@ -75,8 +83,8 @@ const validate = values =>{
 }
 
 const mapStateToProps = (state, ownProps) => {
-    console.log("events-showのmapStateToProps")
-    console.log(state)
+    // console.log("events-showのmapStateToProps")
+    // console.log(state)
     const event = state.events[ownProps.match.params.id]
     // console.log(ownProps.match.params.id)
     return { initialValues: event, state }
